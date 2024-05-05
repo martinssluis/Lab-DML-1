52b135397c9da43bfa1946062ea061c9bd2c81a2
Luis Martins <125165369+martinssluis@users.noreply.github.com>
2024-05-04 22:11:32 -0300

    Add files via upload

---
+++ Lab DML 1.sql
@@ -0,0 +1,89 @@
+/*2.	Quais foram as tabelas geradas?*/
+
+/*
+Resp:
+Disciplina
+Professor
+Aluno
+Classe
+Historico
+*/
+
+
+/*3.	Analisar o conteúdo de cada tabela gerada.*/
+
+select * from Disciplina;
+select * from Professor;
+select * from Aluno;
+select * from Classe;
+select * from Historico;
+
+
+/*4.	Inserir uma disciplina  com código 46280, denominação ‘Segurança de dados’ e nível 10*/
+
+
+
+/*5.	Inserir mais uma disciplina à sua escolha*/
+
+
+
+/*6.	Inserir um aluno de matrícula 171, nome ‘Maria da Enrolação’, sexo ‘F’, idade igual a 23, curso ‘BSI’*/
+
+
+
+/*7.	Inserir mais um aluno à sua escolha*/
+
+
+
+/*8.	Inserir um professor com código 10,  nome ‘João da Silva’ e data de nascimento igual a 18/09/1996.*/
+
+
+
+/*9.	Inserir mais um professor à sua escolha.*/
+
+
+
+/*10.	Inserir um histórico com matrícula 171, disciplina 46267, turma  128, ano_sem ‘1999/2’ e nota 7.5. De quem é este histórico?*/
+
+
+
+/*11.	Inserir mais um histórico à sua escolha. Cuidado com as chaves estrangeiras*/
+
+
+
+/*12.	Visualizar os registros inseridos*/
+
+
+
+/*13.	Atualizar o código da disciplina 46267 para 46269*/
+
+
+
+/*14.	Visualizar os registros da tabela disciplinas. O que aconteceu?*/
+
+
+
+/*15.	Visualizar os registros da tabela classes. O que aconteceu? Por que?*/
+
+
+
+/*16.	Visualizar os registros da tabela historico. O que aconteceu? Por que?*/
+
+
+
+/*17.	A tabela historico possui registro para o aluno 171?*/
+
+
+
+/*18.	Apagar o aluno de matrícula 171 da tabela alunos.*/
+
+
+
+/*19.	Visualizar os registros da tabela alunos. O que aconteceu?*/
+
+
+
+/*20.	Visualizar os registros da tabela historico. O que aconteceu? Por que?*/
+
+
+
